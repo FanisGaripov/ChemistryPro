70a9718065e3c7f3b1674208da7a8ea4c2491668
FanisGaripov <151039847+FanisGaripov@users.noreply.github.com>
2024-11-21 18:12:40 +0300

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,47 +1,47 @@
-const CACHE_NAME = 'my-cache-v1';
-const urlsToCache = [
-    '/',
-    'templates/index.html',
-    // Добавьте другие ресурсы, которые нужно кэшировать, // Пример кэширования изображения
-    // Добавьте другие изображения и CSS-файлы
-];
+// This is the "Offline page" service worker
 
-// Установка service worker и кэширование ресурсов
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Кэшируем ресурсы');
-                return cache.addAll(urlsToCache).catch((error) => {
-                    console.error('Ошибка при кэшировании:', error);
-                });
-            })
-    );
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-// Обработка запросов
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                // Возвращаем кэшированный ресурс или загружаем из сети
-                return response || fetch(event.request);
-            })
-    );
-});
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
 
-// Обновление кэша
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
